15922b913ebd2b33d8e46c9da24f878271c606aa
marica <bottegamarica02@gmail.com>
2023-04-18 14:16:05 +0200

    scemo chi legge

--- src/Marica.java
+++ src/Marica.java
@@ -2,12 +2,12 @@ public class Marica {
     private String name;
     private String surname;
     private int anni;
-    public Marica(String name, String surname, int anni){
+    public Marica(String name, String surname, int anni){ //costruttore
         this.name = name;
         this.surname = surname;
         this.anni = anni;
     }
-
+    //TODO
     public String toString(){
         return name + " " + surname + " " + anni;
     }

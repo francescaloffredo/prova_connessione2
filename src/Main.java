77ad944df1b57da2a958e7623dec25053c3e0603
marica <bottegamarica02@gmail.com>
2023-04-18 14:13:23 +0200

    ho implementato la classe Marica e ho stampato sul main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       //System.out.println("Hello world!");
+        Marica m = new Marica("Marica", "Bottega", 21);
+        System.out.println(m);
     }
 }
--- src/Marica.java
+++ src/Marica.java
@@ -1,2 +1,14 @@
 public class Marica {
+    private String name;
+    private String surname;
+    private int anni;
+    public Marica(String name, String surname, int anni){
+        this.name = name;
+        this.surname = surname;
+        this.anni = anni;
+    }
+
+    public String toString(){
+        return name + " " + surname + " " + anni;
+    }
 }
